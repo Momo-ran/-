88fabd68db4dd59bdf2af94d5a0462ab75147a37
01.py <674434538@qq.com>
2019-07-18 19:31:54 +0800

    by Momo-ran

---
+++ cook_and_code/2-多线程/06-04.py
@@ -0,0 +1,41 @@
+# 使用time延时函数，生成两个函数
+# 利用多线程调用
+# 计算总运行时间
+# 练习带参数的多线程启动方法
+import time
+# 导入多线程包并更名为thread
+import threading
+
+def loop1(in1):
+    # ctime 得到当前时间
+    print('Start loop 1 at:', time.ctime())
+    # 把参数打印出来
+    print(" 我是参数", in1)
+    # 睡眠4秒
+    time.sleep(4)
+    print("End loop 1 at:", time.ctime())
+
+def loop2(in1, in2):
+    # ctime 得到当前时间
+    print('Start loop 2 at:', time.ctime())
+    # 把参数in1 和 in2打印出来,代表使用
+    print(" 我是参数", in1, "和参数", in2)
+    # 睡眠2秒
+    time.sleep(2)
+    print("End loop 2 at:", time.ctime())
+
+def main():
+    print("Starting at:", time.ctime())
+    # 生成threading.Thread实例
+    t1 = threading.Thread(target=loop1, args=("阿南",))
+    t1.start()
+
+    t2= threading.Thread(target=loop2, args=("周震南", "朱一龙"))
+    t2.start()
+
+    print("All done at:", time.ctime())
+
+if __name__ == '__main__':
+    main()
+    while True:
+        time.sleep(10)
